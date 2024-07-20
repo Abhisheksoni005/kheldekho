9af001346633e083c19ba7d9e6fa763838b487cc
Abhishek Soni <abhi5august@gmail.com>
2024-07-21 01:37:24 +0530

    revert ssl changes

--- api/main_api.py
+++ api/main_api.py
@@ -50,6 +50,6 @@ if __name__ == "__main__":
     uvicorn.run(app,
                 host="0.0.0.0",
                 port=8000,
-                # ssl_keyfile=ssl_keyfile,
-                # ssl_certfile=ssl_certfile,
+                ssl_keyfile=ssl_keyfile,
+                ssl_certfile=ssl_certfile,
                 )
