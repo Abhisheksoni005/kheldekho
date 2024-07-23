959af1208c8c00c33b48f416db8e60e10faa0737
Abhishek Soni <abhi5august@gmail.com>
2024-07-23 12:27:54 +0530

    null handling for content

--- api/content_api.py
+++ api/content_api.py
@@ -17,10 +17,14 @@ def get_trivia(date: str, sport: str):
 @router.get("/news/home")
 def get_news(date: str):
     news_response = read_from_json("dataset/news_home.json")
+    if not date in news_response:
+        return JSONResponse(content=[])
     return JSONResponse(content=news_response[date])
 
 
 @router.get("/news/sport")
 def get_sport_news(date: str, sport: str):
     news_response = read_from_json("dataset/news_sport.json")
+    if not date in news_response or not sport in news_response[date]:
+        return JSONResponse(content=[])
     return JSONResponse(content=news_response[date][sport])
